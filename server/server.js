af9c446f57c20174d13a4b13f5e8cee6c7bb2598
blxckpxnther <parthasarathy5117r@gmail.com>
2026-05-06 10:48:13 +0530

    Fix CORS to allow all localhost ports

--- server/server.js
+++ server/server.js
@@ -16,7 +16,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: (origin, callback) => {
+    // Allow localhost on any port or if no origin (like curl requests)
+    if (!origin || /^http:\/\/localhost:\d+$/.test(origin) || /^http:\/\/127\.0\.0\.1:\d+$/.test(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS not allowed'));
+    }
+  },
   credentials: true,
   optionsSuccessStatus: 200
 }));
